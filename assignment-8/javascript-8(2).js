f1802544ceb9f84d6cf9e2bf80e7b075f4c779a4
kruthija <vennukruthija@gmail.com>
2021-09-30 17:01:56 +0530

    commit final changes

---
+++ assignment-8/javascript-8(2).js
@@ -0,0 +1,6 @@
+var fruits=["Apple","Banana","Orange","Guava","PineApple"];
+for( i=0;i<fruits.length;i++)
+{
+    document.write(fruits[i]+"<br />");
+}
+alert("array of fruits");
---
+++ assignment-8/javascript-8(3).js
@@ -0,0 +1,7 @@
+var myname= new String("vennu.Kruthija");
+                 //const i=1;
+                 for(let i=1;i<=100;i++)
+                 {
+                     document.write(myname +"<br/>");
+                 }
+                 alert("printing name for hundred times");
